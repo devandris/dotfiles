a82b26a0ea930491a526315d635977930e06196b
Andras Szanyi <andras.szanyi@finnoconsult.at>
2024-01-17 00:48:27 +0100

    remove __backup

--- private_dot_config/nvim/__backup/ftplugin/json.lua
+++
@@ -1,7 +0,0 @@
-local jsonls_dir = vim.fn.stdpath('data') .. '/mason/packages/eslint-lsp/node_modules/vscode-langservers-extracted/bin'
-
-
-require("lspconfig").jsonls.setup({
-  capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  cmd = { jsonls_dir .. '/vscode-json-language-server', '--stdio' },
-})
